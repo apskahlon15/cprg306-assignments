196ca91077cf2ef860bf884da400042abe3851d0
apskahlon15 <armanpreetsinghk@gmail.com>
2024-05-20 19:10:25 -0600

    My First Commit

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>My Name is Armanpreet Singh Kahlon</p>
+      <Link href={"https://github.com/apskahlon15/cprg306-assignments"}>
+        https://github.com - My Link
+      </Link>
+    </div>
+  );
+}
