937a107e26e1f490a097cf02c97cff34d5559bfa
Armanpreet Singh Kahlon <139932726+apskahlon15@users.noreply.github.com>
2024-05-26 12:15:38 -0600

    Adding week 3 to Page

--- app/page.js
+++ app/page.js
@@ -9,6 +9,9 @@ export default function Home() {
         <li>
           <Link href="./week-2">Week 2 </Link>
         </li>
+        <li>
+          <Link href="./week-3">Week 3</Link>
+        </li>
       </div>
     </main>
   );
