098560cece03f1b34a4a40cd28b0afe2c49d2ff8
Armanpreet Singh Kahlon <139932726+apskahlon15@users.noreply.github.com>
2024-05-26 12:10:42 -0600

    Create page.js

---
+++ app/week-3/page.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ItemList from "./item-list";
+
+const Page = () => {
+  return (
+    <div className="bg-rose-300">
+      <h1
+        className="text-4xl font-extrabold
+      text-black
+      "
+      >
+        Shopping List{" "}
+      </h1>
+      <ItemList />
+    </div>
+  );
+};
+
+export default Page;
